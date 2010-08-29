kuna-systems/buildbot

commit 3f3edec782591d533c45b2527ee6d774a652ce1c

Use a temporary file for FileUpload, and then rename once complete.
Fixes: #161

master/buildbot/steps/transfer.py
@@ -27,7 +27,8 @@ class _FileWriter(pb.Referenceable):
             os.makedirs(dirname)
 
         self.destfile = destfile
-        self.fp = open(destfile, "wb")
+        fd, self.tmpname = tempfile.mkstemp(dir=dirname)
+        self.fp = os.fdopen(fd, 'wb')
         if mode is not None:
             os.chmod(destfile, mode)
         self.remaining = maxsize
@@ -54,6 +55,8 @@ class _FileWriter(pb.Referenceable):
         """
         self.fp.close()
         self.fp = None
+        os.rename(self.tmpname, self.destfile)
+        self.tmpname = None
 
     def __del__(self):
         # unclean shutdown, the file is probably truncated, so delete it
@@ -62,6 +65,8 @@ class _FileWriter(pb.Referenceable):
         if fp:
             fp.close()
             os.unlink(self.destfile)
+            if self.tmpname and os.path.exists(self.tmpname):
+                os.unlink(self.tmpname)
 
 
 def _extractall(self, path=".", members=None):

master/buildbot/test/unit/test_steps_transfer.py
@@ -1,10 +1,50 @@
+import tempfile, os
 from twisted.trial import unittest
 
 from mock import Mock
 
 from buildbot.process.properties import Properties
 from buildbot.util import json
-from buildbot.steps.transfer import StringDownload, JSONStringDownload, JSONPropertiesDownload
+from buildbot.steps.transfer import StringDownload, JSONStringDownload, JSONPropertiesDownload, \
+    FileUpload
+
+class TestFileUpload(unittest.TestCase):
+    def setUp(self):
+        self.fd, self.destfile = tempfile.mkstemp()
+        os.unlink(self.destfile)
+
+    def tearDown(self):
+        os.close(self.fd)
+        os.unlink(self.destfile)
+
+    def testBasic(self):
+        s = FileUpload(slavesrc=__file__, masterdest=self.destfile)
+        s.build = Mock()
+        s.build.getProperties.return_value = Properties()
+        s.build.getSlaveCommandVersion.return_value = 1
+
+        s.step_status = Mock()
+        s.buildslave = Mock()
+        s.remote = Mock()
+
+        s.start()
+
+        for c in s.remote.method_calls:
+            name, command, args = c
+            commandName = command[3]
+            kwargs = command[-1]
+            if commandName == 'uploadFile':
+                self.assertEquals(kwargs['slavesrc'], __file__)
+                writer = kwargs['writer']
+                writer.remote_write(open(__file__, "rb").read())
+                self.assert_(not os.path.exists(self.destfile))
+                writer.remote_close()
+                break
+        else:
+            self.assert_(False, "No uploadFile command found")
+
+        self.assertEquals(open(self.destfile, "rb").read(),
+                open(__file__, "rb").read())
 
 class TestStringDownload(unittest.TestCase):
     def testBasic(self):

commit 9854bc332215c5dffdcc73ec0016f785602e0822

Fix DirectoryUpload
3f3edec broke DirectoryUpload by switching FileUpload to use a
temporary file and doing a rename to move it in place. This
commit updates DirectoryUpload to cope with the new behaviour.

master/buildbot/steps/transfer.py
@@ -107,37 +107,41 @@ def _extractall(self, path=".", members=None):
 class _DirectoryWriter(_FileWriter):
     """
     A DirectoryWriter is implemented as a FileWriter, with an added post-processing
-    step to unpack the archive, once the transfer has completed.  Note that the close()
-    method is not called!
+    step to unpack the archive, once the transfer has completed.
     """
 
     def __init__(self, destroot, maxsize, compress, mode):
         self.destroot = destroot
+        self.compress = compress
 
         self.fd, self.tarname = tempfile.mkstemp()
-        self.compress = compress
+        os.close(self.fd)
+
         _FileWriter.__init__(self, self.tarname, maxsize, mode)
 
     def remote_unpack(self):
         """
         Called by remote slave to state that no more data will be transfered
         """
-        if self.fp:
-            self.fp.close()
-            self.fp = None
-        fileobj = os.fdopen(self.fd, 'rb')
+        # Make sure remote_close is called, otherwise atomic rename wont happen
+        self.remote_close()
+
+        # Map configured compression to a TarFile setting
         if self.compress == 'bz2':
             mode='r|bz2'
         elif self.compress == 'gz':
             mode='r|gz'
         else:
             mode = 'r'
+
+        # Support old python
         if not hasattr(tarfile.TarFile, 'extractall'):
             tarfile.TarFile.extractall = _extractall
-        archive = tarfile.open(name=self.tarname, mode=mode, fileobj=fileobj)
+
+        # Unpack archive and clean up after self
+        archive = tarfile.open(name=self.tarname, mode=mode)
         archive.extractall(path=self.destroot)
         archive.close()
-        fileobj.close()
         os.remove(self.tarname)
 
 