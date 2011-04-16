kuna-systems/buildbot

commit 40dd647aaee3c8b5680146d5a0cb752e9fa23bbf

If the list of builders hasn't changed, don't call remote setBuilderList

master/buildbot/buildslave.py
@@ -89,6 +89,8 @@ class AbstractBuildSlave(pb.Avatar, service.MultiService):
         self.missing_timer = None
         self.keepalive_interval = keepalive_interval
 
+        self._old_builder_list = []
+
     def update(self, new):
         """
         Given a new BuildSlave, configure this one identically.  Because
@@ -429,7 +431,15 @@ class AbstractBuildSlave(pb.Avatar, service.MultiService):
     def sendBuilderList(self):
         our_builders = self.botmaster.getBuildersForSlave(self.slavename)
         blist = [(b.name, b.slavebuilddir) for b in our_builders]
+        if blist == self._old_builder_list:
+            log.msg("Builder list is unchanged; not calling setBuilderList")
+            return defer.succeed(None)
+
         d = self.slave.callRemote("setBuilderList", blist)
+        def sentBuilderList(ign):
+            self._old_builder_list = blist
+            return ign
+        d.addCallback(sentBuilderList)
         return d
 
     def perspective_keepalive(self):
@@ -579,6 +589,9 @@ class BuildSlave(AbstractBuildSlave):
     def sendBuilderList(self):
         d = AbstractBuildSlave.sendBuilderList(self)
         def _sent(slist):
+            # Nothing has changed, so don't need to re-attach to everything
+            if not slist:
+                return
             dl = []
             for name, remote in slist.items():
                 # use get() since we might have changed our mind since then
@@ -821,6 +834,8 @@ class AbstractLatentBuildSlave(AbstractBuildSlave):
     def sendBuilderList(self):
         d = AbstractBuildSlave.sendBuilderList(self)
         def _sent(slist):
+            if not slist:
+                return
             dl = []
             for name, remote in slist.items():
                 # use get() since we might have changed our mind since then.

commit 85c020cd751c6ffc1fe7fb27e099a3c1446948bd

Set _old_builder_list to none when slave disconnects
Commit 40dd647a broke automatic retries, because when the slave
reconnected it was not informed of the builder list if it had not
changed since disconnect.

master/buildbot/buildslave.py
@@ -365,6 +365,7 @@ class AbstractBuildSlave(pb.Avatar, service.MultiService):
 
     def detached(self, mind):
         self.slave = None
+        self._old_builder_list = []
         self.slave_status.removeGracefulWatcher(self._gracefulChanged)
         self.slave_status.setConnected(False)
         log.msg("BuildSlave.detached(%s)" % self.slavename)