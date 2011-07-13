kuna-systems/buildbot

commit 91bd1d7592035db96f91e03e1414334d96d68d9f

Minor changes to Git, Mercurial. Check for installation on slave

master/buildbot/steps/source/git.py
@@ -89,14 +89,14 @@ class Git(Source):
         self.repourl = self.repourl and _ComputeRepositoryURL(self.repourl)
 
     def startVC(self, branch, revision, patch):
-        slavever = self.slaveVersion('git')
-        if not slavever:
-            raise BuildSlaveTooOldError("slave is too old, does not know "
-                                        "about git")
         self.branch = branch or 'master'
         self.revision = revision
         self.method = self._getMethod()
         self.stdio_log = self.addLog("stdio")
+
+        gitInstalled = self.checkGit()
+        if not gitInstalled:
+            raise BuildSlaveTooOldError("git is not installed on slave")
 
         if self.mode == 'incremental':
             d = self.incremental()
@@ -327,6 +327,11 @@ class Git(Source):
         d.addCallback(clobber)
         return d
 
+    def computeSourceRevision(self, changes):
+        if not changes:
+            return None
+        return changes[-1].revision
+
     def _sourcedirIsUpdatable(self):
         cmd = buildstep.LoggedRemoteCommand('stat', {'file': self.workdir + '/.git'})
         log.msg(self.workdir)
@@ -361,3 +366,13 @@ class Git(Source):
             return None
         elif self.method is None and self.mode == 'full':
             return 'fresh'
+
+    def checkGit(self):
+        d = self._dovccmd(['--version'])
+        def check(res):
+            if res == 0:
+                return True
+            return False
+        d.addCallback(check)
+        return d
+

master/buildbot/steps/source/mercurial.py
@@ -98,13 +98,12 @@ class Mercurial(Source):
         self.baseURL = self.baseURL and _ComputeRepositoryURL(self.baseURL)
         
     def startVC(self, branch, revision, patch):
-        
-        slavever = self.slaveVersion('hg')
-        if not slavever:
-            raise BuildSlaveTooOldError("slave is too old, does not know "
-                                        "about hg")
         self.revision = revision
         self.method = self._getMethod()
+        self.stdio_log = self.addLog("stdio")
+        hgInstalled = self.checkHg()
+        if not hgInstalled:
+            raise BuildSlaveTooOldError("Mercurial is not installed on slave")
 
         if self.branchType == 'dirname':
             assert self.repourl is None
@@ -116,8 +115,6 @@ class Mercurial(Source):
             self.update_branch = (branch or 'default')
         else:
             raise ValueError("Invalid branch type")
-        
-        self.stdio_log = self.addLog("stdio")
 
         if self.mode == 'full':
             d = self.full()
@@ -256,6 +253,19 @@ class Mercurial(Source):
         d.addCallback(lambda _: evaluateCommand(cmd))
         return d
 
+    def computeSourceRevision(self, changes):
+        if not changes:
+            return None
+        # without knowing the revision ancestry graph, we can't sort the
+        # changes at all. So for now, assume they were given to us in sorted
+        # order, and just pay attention to the last one. See ticket #103 for
+        # more details.
+        if len(changes) > 1:
+            log.msg("Mercurial.computeSourceRevision: warning: "
+                    "there are %d changes here, assuming the last one is "
+                    "the most recent" % len(changes))
+        return changes[-1].revision
+
     def _getCurrentBranch(self):
         if self.branchType == 'dirname':
             return defer.succeed(self.branch)
@@ -292,3 +302,13 @@ class Mercurial(Source):
             command += ['--rev', self.revision]
         d = self._dovccmd(command)
         return d
+
+    def checkHg(self):
+        d = self._dovccmd(['--version'])
+        def check(res):
+            if res == 0:
+                return True
+            return False
+        d.addCallback(check)
+        return d
+

master/buildbot/test/unit/test_steps_source_git.py
@@ -33,6 +33,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                 git.Git(repourl='http://github.com/buildbot/buildbot.git',
                                     mode='full', method='clean'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.git'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -60,6 +63,10 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                 git.Git(repourl='http://github.com/buildbot/buildbot.git',
                                     mode='full', method='clean'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
+
             ExpectLogged('stat', dict(file='wkdir/.git'))
             + 1,
             ExpectShell(workdir='wkdir',
@@ -81,6 +88,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                         mode='full', method='clobber', progress=True))
 
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('rmdir', dict(dir='wkdir'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -102,6 +112,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                 git.Git(repourl='http://github.com/buildbot/buildbot.git',
                                     mode='incremental'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.git'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -127,6 +140,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                 git.Git(repourl='http://github.com/buildbot/buildbot.git',
                                     mode='full', method='fresh'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.git'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -156,6 +172,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                 revision='abcdef01',
                 ))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.git'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -178,6 +197,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                 git.Git(repourl='http://github.com/buildbot/buildbot.git',
                                     mode='full', method='fresh', submodule=True))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.git'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -213,6 +235,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                         mode='full', method='clobber', shallow=True))
 
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('rmdir', dict(dir='wkdir'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -235,6 +260,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                         mode='incremental', retryFetch=True))
 
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.git'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -268,6 +296,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                         mode='incremental', clobberOnFailure=True))
 
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.git'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -300,6 +331,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                         mode='full', method='copy', shallow=True))
 
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('rmdir', dict(dir='wkdir')),
             ExpectLogged('stat', dict(file='source/.git'))
             + 0,
@@ -328,6 +362,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                 git.Git(repourl='http://github.com/buildbot/buildbot.git',
                                     mode='incremental'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.git'))
             + 1,
             ExpectShell(workdir='wkdir',
@@ -351,6 +388,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                 revision='abcdef01',
                 ))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('rmdir', dict(dir='wkdir'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -376,6 +416,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                         mode='full', method='clobber', submodule=True))
 
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('rmdir', dict(dir='wkdir'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -406,6 +449,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                 revision='abcdef01',
                 ))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.git'))
             + 1,
             ExpectShell(workdir='wkdir',
@@ -431,6 +477,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                         mode='full', method='fresh', clobberOnFailure=True))
 
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.git'))
             + 1,
             ExpectShell(workdir='wkdir',
@@ -458,6 +507,9 @@ class TestGit(sourcesteps.SourceStepMixin, unittest.TestCase):
                 git.Git(repourl='http://github.com/buildbot/buildbot.git',
                                     mode='full'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['git', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.git'))
             + 0,
             ExpectShell(workdir='wkdir',

master/buildbot/test/unit/test_steps_source_mercurial.py
@@ -41,6 +41,9 @@ class TestMercurial(sourcesteps.SourceStepMixin, unittest.TestCase):
                 mercurial.Mercurial(repourl='http://hg.mozilla.org',
                                     mode='full', method='clean', branchType='inrepo'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['hg', '--verbose', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.hg'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -76,6 +79,9 @@ class TestMercurial(sourcesteps.SourceStepMixin, unittest.TestCase):
                 mercurial.Mercurial(repourl='http://hg.mozilla.org',
                                     mode='full', method='clean', branchType='inrepo'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['hg', '--verbose', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.hg'))
             + 1,
             ExpectShell(workdir='wkdir',
@@ -98,6 +104,9 @@ class TestMercurial(sourcesteps.SourceStepMixin, unittest.TestCase):
                 mercurial.Mercurial(repourl='http://hg.mozilla.org',
                                     mode='full', method='clobber', branchType='inrepo'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['hg', '--verbose', '--version'])
+            + 0,
             ExpectLogged('rmdir', dict(dir='wkdir'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -123,6 +132,9 @@ class TestMercurial(sourcesteps.SourceStepMixin, unittest.TestCase):
                 mercurial.Mercurial(repourl='http://hg.mozilla.org',
                                     mode='full', method='fresh', branchType='inrepo'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['hg', '--verbose', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.hg'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -158,6 +170,9 @@ class TestMercurial(sourcesteps.SourceStepMixin, unittest.TestCase):
                 mercurial.Mercurial(repourl='http://hg.mozilla.org',
                                     mode='full', method='fresh', branchType='inrepo'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['hg', '--verbose', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.hg'))
             + 1,
             ExpectShell(workdir='wkdir',
@@ -181,6 +196,9 @@ class TestMercurial(sourcesteps.SourceStepMixin, unittest.TestCase):
                                     mode='incremental', branchType='dirname'),
             )
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['hg', '--verbose', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.hg'))
             + 1, # does not exist
             ExpectShell(workdir='wkdir',
@@ -209,6 +227,9 @@ class TestMercurial(sourcesteps.SourceStepMixin, unittest.TestCase):
             dict(branch='stable')
             )
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['hg', '--verbose', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.hg'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -241,6 +262,9 @@ class TestMercurial(sourcesteps.SourceStepMixin, unittest.TestCase):
                 mercurial.Mercurial(repourl='http://hg.mozilla.org',
                                     mode='incremental', branchType='inrepo'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['hg', '--verbose', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.hg'))
             + 1, # does not exist
             ExpectShell(workdir='wkdir',
@@ -270,6 +294,9 @@ class TestMercurial(sourcesteps.SourceStepMixin, unittest.TestCase):
                 mercurial.Mercurial(repourl='http://hg.mozilla.org',
                                     mode='incremental', branchType='inrepo'))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['hg', '--verbose', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.hg'))
             + 0, # directory exists
             ExpectShell(workdir='wkdir',
@@ -302,6 +329,9 @@ class TestMercurial(sourcesteps.SourceStepMixin, unittest.TestCase):
                 ))
 
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['hg', '--verbose', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.hg'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -334,6 +364,9 @@ class TestMercurial(sourcesteps.SourceStepMixin, unittest.TestCase):
                 branch='stable',
                 ))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['hg', '--verbose', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.hg'))
             + 0,
             ExpectShell(workdir='wkdir',
@@ -373,6 +406,9 @@ class TestMercurial(sourcesteps.SourceStepMixin, unittest.TestCase):
                 branch='stable',
                 ))
         self.expectCommands(
+            ExpectShell(workdir='wkdir',
+                        command=['hg', '--verbose', '--version'])
+            + 0,
             ExpectLogged('stat', dict(file='wkdir/.hg'))
             + 0,
             ExpectShell(workdir='wkdir',

commit 95a666646bc341eca48d417c03b4aff0c9469b35

Fix bug introduced in 91bd1d7

master/buildbot/steps/source/git.py
@@ -94,14 +94,18 @@ class Git(Source):
         self.method = self._getMethod()
         self.stdio_log = self.addLog("stdio")
 
-        gitInstalled = self.checkGit()
-        if not gitInstalled:
-            raise BuildSlaveTooOldError("git is not installed on slave")
+        d = self.checkGit()
+        def checkInstall(gitInstalled):
+            if not gitInstalled:
+                raise BuildSlaveTooOldError("git is not installed on slave")
+            return 0
+        d.addCallback(checkInstall)
+
 
         if self.mode == 'incremental':
-            d = self.incremental()
+            d.addCallback(lambda _: self.incremental())
         elif self.mode == 'full':
-            d = self.full()
+            d.addCallback(lambda _: self.full())
         d.addCallback(self.parseGotRevision)
         d.addCallback(self.finish)
         d.addErrback(self.failed)
@@ -375,4 +379,3 @@ class Git(Source):
             return False
         d.addCallback(check)
         return d
-

master/buildbot/steps/source/mercurial.py
@@ -101,9 +101,11 @@ class Mercurial(Source):
         self.revision = revision
         self.method = self._getMethod()
         self.stdio_log = self.addLog("stdio")
-        hgInstalled = self.checkHg()
-        if not hgInstalled:
-            raise BuildSlaveTooOldError("Mercurial is not installed on slave")
+        d = self.checkHg()
+        def checkInstall(hgInstalled):
+            if not hgInstalled:
+                raise BuildSlaveTooOldError("Mercurial is not installed on slave")
+            return 0
 
         if self.branchType == 'dirname':
             assert self.repourl is None
@@ -117,9 +119,9 @@ class Mercurial(Source):
             raise ValueError("Invalid branch type")
 
         if self.mode == 'full':
-            d = self.full()
+            d.addCallback(lambda _: self.full())
         elif self.mode == 'incremental':
-            d = self.incremental()
+            d.addCallback(lambda _: self.incremental())
         d.addCallback(self.parseGotRevision)
         d.addCallback(self.finish)
         d.addErrback(self.failed)