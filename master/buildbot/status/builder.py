kuna-systems/buildbot

commit 20a8fc099afef3f0ae5e4aff52bed17c2cf2ce82

Fix potential KeyError in build cache
Fixes #2162.

master/buildbot/status/builder.py
@@ -177,10 +177,14 @@ class BuilderStatus(styles.Versioned):
                 return self.touchBuildCache(b)
 
         # then in the buildCache
-        if number in self.buildCache:
-            metrics.MetricCountEvent.log("buildCache.hits", 1)
-            return self.touchBuildCache(self.buildCache[number])
-        metrics.MetricCountEvent.log("buildCache.misses", 1)
+        try:
+            b = self.buildCache[number]
+        except KeyError:
+            metrics.MetricCountEvent.log("buildCache.misses", 1)
+            return
+
+        metrics.MetricCountEvent.log("buildCache.hits", 1)
+        return self.touchBuildCache(b)
 
         # then fall back to loading it from disk
         filename = self.makeBuildFilename(number)

commit 5a9dfea2e92cebcd960d9dec0d12cba47673f8cd

load builds from disk on RAM cache miss
Fixes a (stupid) bug introduced in
20a8fc099afef3f0ae5e4aff52bed17c2cf2ce82.  Fixes #2163.

master/buildbot/status/builder.py
@@ -181,10 +181,9 @@ class BuilderStatus(styles.Versioned):
             b = self.buildCache[number]
         except KeyError:
             metrics.MetricCountEvent.log("buildCache.misses", 1)
-            return
-
-        metrics.MetricCountEvent.log("buildCache.hits", 1)
-        return self.touchBuildCache(b)
+        else:
+            metrics.MetricCountEvent.log("buildCache.hits", 1)
+            return self.touchBuildCache(b)
 
         # then fall back to loading it from disk
         filename = self.makeBuildFilename(number)