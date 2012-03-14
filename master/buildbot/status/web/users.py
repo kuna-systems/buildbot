kuna-systems/buildbot

commit 43eb37b06ce5c75db9e7c6140782bde05b5a57a0

authz: rework for cookie based session management
Instead of requiring the user to type password in each and
every form inside buildbot, we implement a simple session
manager.

Existing IAuth interface is augmented with getUserInfos()
which is used to display info about the user
/login and /logout ActionResources are added to manage login and logout

template helpers like advertiseAction() now needs to pass request, so
that authz can get the session cookie from headers

now that we can identify between auth fail and authz fail,
we also add an authz failed page in order to let the user know
whether the authentication failed of the authorization failed.

These changes happen behind the scene, there is no update in the
configuration API, and the user should just notice the change in the
UI (the login form changed)

Signed-off-by: Pierre Tardy <pierre.tardy@intel.com>

master/buildbot/status/web/users.py
@@ -16,7 +16,7 @@
 import urllib
 from twisted.internet import defer
 from twisted.web.util import redirectTo
-from buildbot.status.web.base import HtmlResource, path_to_authfail, \
+from buildbot.status.web.base import HtmlResource, path_to_authzfail, \
     path_to_root, ActionResource
 
 class UsersActionResource(ActionResource):
@@ -31,7 +31,7 @@ class UsersActionResource(ActionResource):
         yield wfd
         res = wfd.getResult()
         if not res:
-            yield path_to_authfail(req)
+            yield path_to_authzfail(req)
             return
         # show the table
         yield path_to_root(req) + "users/table"
@@ -81,7 +81,7 @@ class UsersTableResource(HtmlResource):
         yield wfd
         res = wfd.getResult()
         if not res:
-            yield redirectTo(path_to_authfail(req), req)
+            yield redirectTo(path_to_authzfail(req), req)
             return
 
         s = self.getStatus(req)
@@ -120,7 +120,7 @@ class UsersResource(HtmlResource):
                 yield redirectTo("users/table", req)
                 return
             else:
-                yield redirectTo(path_to_authfail(req), req)
+                yield redirectTo(path_to_authzfail(req), req)
                 return
 
         ctx['authz'] = self.getAuthz(req)

master/buildbot/status/web/auth.py
@@ -16,7 +16,8 @@
 
 import os
 from zope.interface import Interface, Attribute, implements
-from buildbot.status.web.base import HtmlResource
+from buildbot.status.web.base import HtmlResource, ActionResource, path_to_authfail
+
 from buildbot.process.users import users
 
 class IAuth(Interface):
@@ -32,6 +33,11 @@ class IAuth(Interface):
     def authenticate(self, user, passwd):
             """Check whether C{user} / C{passwd} are valid."""
 
+    def getUserInfo(self, user):
+            """return dict with user info.
+            dict( fullName="", email="", groups=[])
+            """
+
     def errmsg(self):
             """Get the reason authentication failed."""
 
@@ -41,6 +47,10 @@ class AuthBase:
 
     def errmsg(self):
         return self.err
+
+    def getUserInfo(self, user):
+        """default dummy impl"""
+        return dict(userName=user, fullName=user, email=user+"@buildbot.net", groups=[ user ])
 
 class BasicAuth(AuthBase):
     implements(IAuth)
@@ -140,4 +150,27 @@ class AuthFailResource(HtmlResource):
     def content(self, request, cxt):
         template = request.site.buildbot_service.templates.get_template("authfail.html")
         return template.render(**cxt)
+class AuthzFailResource(HtmlResource):
+    pageTitle = "Authorization Failed"
+    def content(self, request, cxt):
+        template = request.site.buildbot_service.templates.get_template("authzfail.html")
+        return template.render(**cxt)
     
+class LoginResource(ActionResource):
+    def performAction(self, request):
+        authz = self.getAuthz(request)
+        d = authz.login(request)
+        def on_login(res):
+            if res:
+                root = request.site.buildbot_service.master.status.getBuildbotURL()
+                return request.requestHeaders.getRawHeaders('referer',[root])[0]
+            else:
+                return path_to_authfail(request)
+        d.addBoth(on_login)
+        return d
+class LogoutResource(ActionResource):
+    def performAction(self, request):
+        authz = self.getAuthz(request)
+        authz.logout(request)
+        root = request.site.buildbot_service.master.status.getBuildbotURL()
+        return request.requestHeaders.getRawHeaders('referer',[root])[0]

master/buildbot/status/web/authz.py
@@ -15,7 +15,9 @@
 
 from twisted.internet import defer
 from buildbot.status.web.auth import IAuth
+from buildbot.status.web.session import SessionManager
 
+COOKIE_KEY="BuildBotSession"
 class Authz(object):
     """Decide who can do what."""
 
@@ -51,48 +53,71 @@ class Authz(object):
                 self.config[act] = kwargs[act]
                 del kwargs[act]
 
+        self.sessions = SessionManager()
         if kwargs:
             raise ValueError("unknown authorization action(s) " + ", ".join(kwargs.keys()))
 
+    def session(self, request):
+        if COOKIE_KEY in request.received_cookies:
+            cookie = request.received_cookies[COOKIE_KEY]
+            return self.sessions.get(cookie)
+        return None
+            
+    def authenticated(self, request):
+        if self.useHttpHeader:
+            return request.getUser() != None
+        return self.session(request) != None
+
+    def getUserInfo(self, user):
+        if self.useHttpHeader:
+            return dict(userName=user, fullName=user, email=user, groups=[ user ])
+        s = self.sessions.getUser(user)
+        if s:
+            return s.infos
+
     def getUsername(self, request):
+        """Get the userid of the user"""
         if self.useHttpHeader:
             return request.getUser()
+        s = self.session(request)
+        if s:
+            return s.user
         return request.args.get("username", ["<unknown>"])[0]
+
+    def getUsernameHTML(self, request):
+        """Get the user formatated in html (with possible link to email)"""
+        if self.useHttpHeader:
+            return request.getUser()
+        s = self.session(request)
+        if s:
+            return s.userInfosHTML()
+        return "not authenticated?!"
+
+    def getUsernameFull(self, request):
+        """Get the full username as fullname <email>"""
+        if self.useHttpHeader:
+            return request.getUser()
+        s = self.session(request)
+        if s:
+            return "%(fullName)s <%(email)s>"%(s.infos)
+        else:
+            return request.args.get("username", ["<unknown>"])[0]
+
 
     def getPassword(self, request):
         if self.useHttpHeader:
             return request.getPassword()
         return request.args.get("passwd", ["<no-password>"])[0]
 
-    def advertiseAction(self, action):
+    def advertiseAction(self, action, request):
         """Should the web interface even show the form for ACTION?"""
         if action not in self.knownActions:
             raise KeyError("unknown action")
         cfg = self.config.get(action, False)
         if cfg:
-            return True
-        return False
-
-    def needAuthForm(self, action):
-        """Does this action require an authentication form?"""
-        if action not in self.knownActions:
-            raise KeyError("unknown action")
-        cfg = self.config.get(action, False)
-        if cfg == 'auth' or callable(cfg):
-            return True
-        return False
-
-    def needUserForm(self, action):
-        """Does this action require an user form?"""
-        if action not in self.knownActions:
-            raise KeyError("unknown action")
-        if self.useHttpHeader:
-            # TODO: show the form when Authorization header is missing?
-            return False
-        cfg = self.config.get(action, False)
-        if cfg:
-            return True
-        return False
+            if cfg == 'auth' or callable(cfg):
+                return self.authenticated(request)
+        return cfg
 
     def actionAllowed(self, action, request, *args):
         """Is this ACTION allowed, given this http REQUEST?"""
@@ -103,20 +128,51 @@ class Authz(object):
             if cfg == 'auth' or callable(cfg):
                 if not self.auth:
                     return defer.succeed(False)
-                user = self.getUsername(request)
+                def check_authenticate(res):
+                    if callable(cfg) and not cfg(self.getUsername(request), *args):
+                        return False
+                    return True
+                # retain old behaviour, if people have scripts
+                # without cookie support
                 passwd = self.getPassword(request)
-                if user == "<unknown>" or passwd == "<no-password>":
+                if self.authenticated(request):
+                    return defer.succeed(check_authenticate(None))
+                elif passwd != "<no-password>":
+                    def check_login(cookie):
+                        ret = False
+                        if type(cookie) is str:
+                            ret = check_authenticate(None)
+                            self.sessions.remove(cookie)
+                        return ret
+                    d = self.login(request)
+                    d.addBoth(check_login)
+                    return d
+                else:
                     return defer.succeed(False)
-
-                d = defer.maybeDeferred(self.auth.authenticate, user, passwd)
-                def check_authenticate(res, cfg, user, *args):
-                    if res:
-                        if callable(cfg) and not cfg(user, *args):
-                            return False
-                        return True
-                    return False
-                d.addCallback(check_authenticate, cfg, user, *args)
-                return d
+        return defer.succeed(cfg)
+    def login(self, request):
+        """Login one user, and return session cookie"""
+        if self.authenticated(request):
+            return defer.succeed(False)
+            
+        user = request.args.get("username", ["<unknown>"])[0]
+        passwd = request.args.get("passwd", ["<no-password>"])[0]
+        if user == "<unknown>" or passwd == "<no-password>":
+            return defer.succeed(False)
+        if not self.auth:
+            return defer.succeed(False)
+        d = defer.maybeDeferred(self.auth.authenticate, user, passwd)
+        def check_authenticate(res):
+            if res:
+                cookie, s = self.sessions.new(user, self.auth.getUserInfo(user))
+                request.addCookie(COOKIE_KEY, cookie, s.getExpiration(),path="/")
+                request.received_cookies = {COOKIE_KEY:cookie}
+                return cookie
             else:
-                return defer.succeed(True) # anyone can do this..
-        return defer.succeed(False)
+                return False
+        d.addBoth(check_authenticate)
+        return d
+    def logout(self, request):
+        if COOKIE_KEY in request.received_cookies:
+            cookie = request.received_cookies[COOKIE_KEY]
+            self.sessions.remove(cookie)

master/buildbot/status/web/base.py
@@ -123,6 +123,9 @@ def path_to_root(request):
 def path_to_authfail(request):
     return path_to_root(request) + "authfail"
 
+def path_to_authzfail(request):
+    return path_to_root(request) + "authzfail"
+
 def path_to_builder(request, builderstatus):
     return (path_to_root(request) +
             "builders/" +
@@ -209,6 +212,7 @@ class ContextMixin(AccessorMixin):
                     pageTitle = self.getPageTitle(request),
                     welcomeurl = rootpath,
                     authz = self.getAuthz(request),
+                    request = request,
                     )
 
 

master/buildbot/status/web/baseweb.py
@@ -39,7 +39,7 @@ from buildbot.status.web.slaves import BuildSlavesResource
 from buildbot.status.web.status_json import JsonStatusResource
 from buildbot.status.web.about import AboutBuildbot
 from buildbot.status.web.authz import Authz
-from buildbot.status.web.auth import AuthFailResource
+from buildbot.status.web.auth import AuthFailResource,AuthzFailResource, LoginResource, LogoutResource
 from buildbot.status.web.root import RootPage
 from buildbot.status.web.users import UsersResource
 from buildbot.status.web.change_hook import ChangeHookResource
@@ -360,7 +360,10 @@ class WebStatus(service.MultiService):
                       OneLinePerBuild(numbuilds=numbuilds))
         self.putChild("about", AboutBuildbot())
         self.putChild("authfail", AuthFailResource())
+        self.putChild("authzfail", AuthzFailResource())
         self.putChild("users", UsersResource())
+        self.putChild("login", LoginResource())
+        self.putChild("logout", LogoutResource())
 
     def __repr__(self):
         if self.http_port is None:

master/buildbot/status/web/build.py
@@ -22,8 +22,8 @@ import urllib, time
 from twisted.python import log
 from buildbot.status.web.base import HtmlResource, \
      css_classes, path_to_build, path_to_builder, path_to_slave, \
-     getAndCheckProperties, ActionResource, path_to_authfail
-
+     getAndCheckProperties, ActionResource, path_to_authzfail
+from buildbot.schedulers.forcesched import ForceSched, TextParameter
 from buildbot.status.web.step import StepsResource
 from buildbot.status.web.tests import TestsResource
 from buildbot import util, interfaces
@@ -45,7 +45,7 @@ class ForceBuildActionResource(ActionResource):
         res = wfd.getResult()
 
         if not res:
-            url = path_to_authfail(req)
+            url = path_to_authzfail(req)
         else:
             # get a control object
             c = interfaces.IControl(self.getBuildmaster(req))
@@ -54,7 +54,7 @@ class ForceBuildActionResource(ActionResource):
             b = self.build_status
             builder_name = self.builder.getName()
             log.msg("web rebuild of build %s:%s" % (builder_name, b.getNumber()))
-            name = authz.getUsername(req)
+	    name =authz.getUsernameFull(req)
             comments = req.args.get("comments", ["<no reason specified>"])[0]
             reason = ("The web-page 'rebuild' button was pressed by "
                       "'%s': %s\n" % (name, comments))
@@ -101,13 +101,13 @@ class StopBuildActionResource(ActionResource):
         res = wfd.getResult()
 
         if not res:
-            yield path_to_authfail(req)
+            yield path_to_authzfail(req)
             return
 
         b = self.build_status
         log.msg("web stopBuild of build %s:%s" % \
                     (b.getBuilder().getName(), b.getNumber()))
-        name = authz.getUsername(req)
+        name = authz.getUsernameFull(req)
         comments = req.args.get("comments", ["<no reason specified>"])[0]
         # html-quote both the username and comments, just to be safe
         reason = ("The web-page 'stop build' button was pressed by "
@@ -250,7 +250,8 @@ class StatusResourceBuild(HtmlResource):
         b = self.build_status
         log.msg("web stopBuild of build %s:%s" % \
                 (b.getBuilder().getName(), b.getNumber()))
-        name = self.getAuthz(req).getUsername(req)
+
+        name = self.getAuthz(req).getUsernameFull(req)
         comments = req.args.get("comments", ["<no reason specified>"])[0]
         # html-quote both the username and comments, just to be safe
         reason = ("The web-page 'stop build' button was pressed by "

master/buildbot/status/web/builder.py
@@ -23,7 +23,7 @@ from buildbot import interfaces
 from buildbot.status.web.base import HtmlResource, BuildLineMixin, \
     path_to_build, path_to_slave, path_to_builder, path_to_change, \
     path_to_root, getAndCheckProperties, ICurrentBox, build_get_class, \
-    map_branches, path_to_authfail, ActionResource
+    map_branches, path_to_authzfail, ActionResource
 
 from buildbot.status.web.build import BuildsResource, StatusResourceBuild
 from buildbot import util
@@ -44,7 +44,7 @@ class ForceAllBuildsActionResource(ActionResource):
         res = wfd.getResult()
 
         if not res:
-            yield path_to_authfail(req)
+            yield path_to_authzfail(req)
             return
 
         builders = None
@@ -76,7 +76,7 @@ class StopAllBuildsActionResource(ActionResource):
         yield wfd
         res = wfd.getResult()
         if not res:
-            yield path_to_authfail(req)
+            yield path_to_authzfail(req)
             return
 
         builders = None
@@ -114,7 +114,7 @@ class PingBuilderActionResource(ActionResource):
         res = wfd.getResult()
         if not res:
             log.msg("..but not authorized")
-            yield path_to_authfail(req)
+            yield path_to_authzfail(req)
             return
 
         c = interfaces.IControl(self.getBuildmaster(req))
@@ -140,7 +140,7 @@ class ForceBuildActionResource(ActionResource):
         res = wfd.getResult()
         if not res:
             log.msg("..but not authorized")
-            yield path_to_authfail(req)
+            yield path_to_authzfail(req)
             return
 
         master = self.getBuildmaster(req)
@@ -403,7 +403,7 @@ class CancelChangeResource(ActionResource):
                     if res:
                         build_req.cancel()
                     else:
-                        yield path_to_authfail(req)
+                        yield path_to_authzfail(req)
                         return
                     if not cancel_all:
                         break
@@ -478,7 +478,7 @@ class StopChangeResource(StopChangeMixin, ActionResource):
         success = wfd.getResult()
 
         if not success:
-            yield path_to_authfail(req)
+            yield path_to_authzfail(req)
         else:
             yield path_to_builder(req, self.builder_status)
 
@@ -497,7 +497,7 @@ class StopChangeAllResource(StopChangeMixin, ActionResource):
         yield wfd
         res = wfd.getResult()
         if not res:
-            yield path_to_authfail(req)
+            yield path_to_authzfail(req)
             return
 
         for bname in self.status.getBuilderNames():
@@ -506,7 +506,7 @@ class StopChangeAllResource(StopChangeMixin, ActionResource):
                 self.stopChangeForBuilder(req, builder_status, auth_ok=True))
             yield wfd
             if not wfd.getResult():
-                yield path_to_authfail(req)
+                yield path_to_authzfail(req)
                 return
 
         yield path_to_root(req)

master/buildbot/status/web/files/default.css
@@ -11,6 +11,22 @@ body.interface {
 	color: #333;
 }
 
+.auth {
+position:absolute;
+top:5px;
+right:40px;
+}
+
+.alert {
+  color: #c30000;
+  background-color: #f2dcdc;
+  padding: 5px 5px 5px 25px;
+  margin-bottom: 20px;
+  border-top:1px solid #ccc;
+  border-bottom:1px solid #ccc;
+  border-color: #c30000;
+  font-size: 20px;
+}
 a:link,a:visited,a:active {
 	color: #444;
 }

commit 256104ea1acb48e4ac1b0a26f9a60170d31c9b2d

web: replace the content of the /users page with the one of the /users/table
The /users page was anyway broken since
43eb37b06ce5c75db9e7c6140782bde05b5a57a0

master/buildbot/status/web/templates/users.html
@@ -1,12 +1,27 @@
 {% extends "layout.html" %}
-{% import 'forms.html' as forms %}
 
 {% block content %}
 
 <h1>Users</h1>
 
-{%- if authz.needAuthForm('showUsersPage') -%}
-  {{ forms.show_users(table_link, authz) }}
-{% endif %}
+<table class="info">
+
+<tr>
+  <th>Uid</th>
+  <th>Identifier</th>
+</tr>
+
+{% for user in users %}
+  <tr class="{{ loop.cycle('alt','') }}">
+
+  <td><b><a href="{{ user.user_link }}">{{ user.uid }}</a></b></td>
+  <td>{{ user.identifier|e }}</td>
+
+  </tr>
+{% endfor %}
+
+</table>
+
+</div>
 
 {% endblock %}

master/buildbot/status/web/templates/users_table.html
@@ -1,27 +1,0 @@
-{% extends "layout.html" %}
-
-{% block content %}
-
-<h1>Users</h1>
-
-<table class="info">
-
-<tr>
-  <th>Uid</th>
-  <th>Identifier</th>
-</tr>
-
-{% for user in users %}
-  <tr class="{{ loop.cycle('alt','') }}">
-
-  <td><b><a href="{{ user.user_link }}">{{ user.uid }}</a></b></td>
-  <td>{{ user.identifier|e }}</td>
-
-  </tr>
-{% endfor %}
-
-</table>
-
-</div>
-
-{% endblock %}

master/buildbot/status/web/users.py
@@ -34,7 +34,7 @@ class UsersActionResource(ActionResource):
             yield path_to_authzfail(req)
             return
         # show the table
-        yield path_to_root(req) + "users/table"
+        yield path_to_root(req) + "users"
 
 # /users/$uid
 class OneUserResource(HtmlResource):
@@ -63,9 +63,9 @@ class OneUserResource(HtmlResource):
         d.addCallback(cb)
         return d
 
-# /users/table
-class UsersTableResource(HtmlResource):
-    pageTitle = "Users Table"
+# /users
+class UsersResource(HtmlResource):
+    pageTitle = "Users"
     addSlash = True
 
     def __init__(self):
@@ -95,35 +95,5 @@ class UsersTableResource(HtmlResource):
         for user in users:
             user['user_link'] = req.childLink(urllib.quote(str(user['uid']), ''))
         template = req.site.buildbot_service.templates.get_template(
-                                                              "users_table.html")
-        yield template.render(**ctx)
-
-# /users
-class UsersResource(HtmlResource):
-    pageTitle = "Users"
-    addSlash = True
-
-    def __init__(self):
-        HtmlResource.__init__(self)
-        self.action = "showUsersPage"
-
-    def getChild(self, path, req):
-        if path == "table":
-            return UsersTableResource()
-
-    @defer.deferredGenerator
-    def content(self, req, ctx):
-        # check for False or True on showUsersPage, redirect immediately
-        authz = self.getAuthz(req)
-        if not authz.needAuthForm(self.action):
-            if authz.advertiseAction(self.action):
-                yield redirectTo("users/table", req)
-                return
-            else:
-                yield redirectTo(path_to_authzfail(req), req)
-                return
-
-        ctx['authz'] = self.getAuthz(req)
-        ctx['table_link'] = req.childLink("table")
-        template = req.site.buildbot_service.templates.get_template("users.html")
+                                                              "users.html")
         yield template.render(**ctx)